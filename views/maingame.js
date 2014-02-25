bb106eea17d0a26ef4a9d3f273c171d7a5f1ebe5
santi8ago8 <santi8ago8@gmail.com>
2014-02-25 20:23:20 -0300

    creating the structure 2

---
+++ views/maingame.js
@@ -0,0 +1,6 @@
+function template(locals) {
+var buf = [];
+var jade_mixins = {};
+var locals_ = (locals || {}),b = locals_.b;
+buf.push("<!--Created by santiago on 25/02/14.\n--><h2>fondo " + (jade.escape((jade.interp = b) == null ? '' : jade.interp)) + "</h2><h3>fomdo\n<img src='hola.jpg'/></h3>");;return buf.join("");
+}

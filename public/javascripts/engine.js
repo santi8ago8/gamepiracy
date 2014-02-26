246408842abebbc7db865d6890d496fb86e1c73d
santi8ago8 <santi8ago8@gmail.com>
2014-02-26 02:12:03 -0300

    create 3 levels

--- public/javascripts/engine.js
+++ public/javascripts/engine.js
@@ -54,6 +54,23 @@ var Game = function () {
     var decclick = 3;
     var intPiracy = b.Array();
     var initDate;
+    var template = jade.compile(b.u.qs('#main').html());
+    var templatePerson = jade.compile(b.u.qs('#person1').html());
+    var step = 0;
+    var anchoW = 0;
+    var anchoCantante = 0;
+    var cantante;
+    var bajista;
+    var batero;
+
+    var resizeWindow = function () {
+        anchoW = content.f().offsetWidth;
+
+        console.log(anchoW);
+    };
+    resizeWindow();
+    window.addEventListener('resize', resizeWindow);
+
     var load = function () {
 
         content.html(template());
@@ -71,6 +88,7 @@ var Game = function () {
         initGame();
     };
     var initGame = function () {
+        step = 0;
         content.qs('.start').hide();
         content.f().removeEventListener('click', startListener);
 
@@ -85,6 +103,13 @@ var Game = function () {
         }, 1000));
         intPiracy.push(setInterval(movePiracy, 100));
         content.f().addEventListener('click', backPiracy);
+
+        content.qs('.persons').html(templatePerson());
+
+        cantante = content.qs('.cantante');
+        bajista = content.qs('.bajista');
+        batero = content.qs('.batero');
+
         initDate = Date.now();
     };
     var movePiracy = function () {
@@ -96,7 +121,7 @@ var Game = function () {
             clearInterval(i);
         }
         content.f().removeEventListener('click', backPiracy);
-        updatePiracy(1000);
+        piracy.css('right', '100%');
         b.u.debug('end', finalScore);
     };
     var backPiracy = function () {
@@ -106,7 +131,6 @@ var Game = function () {
         currentPiracy += n;
         if (currentPiracy < 0) {
             end();
-            piracy.css('right', '0%');
         } else {
             if (currentPiracy > 100)
                 currentPiracy = 99;
@@ -116,14 +140,56 @@ var Game = function () {
     };
     var updateMusicians = function () {
 
+        var restScreen = anchoW - (anchoW * ((100 - currentPiracy) / 100));
+        var piracyScreen = anchoW - restScreen;
+
         if (currentPiracy > 75) {
             //completos
 
-        } else if (currentPiracy > 50) {
+
+            var anchoCantante = cantante.f().offsetWidth;
+            cantante.css('left', (piracyScreen + (restScreen / 2)) - (anchoCantante / 2) + 'px');
+
+            var anchoBajista = bajista.f().offsetWidth;
+            bajista.css('left', (piracyScreen + (restScreen / 4)) - (anchoBajista / 2) + 'px');
+
+            var anchoBatero = batero.f().offsetWidth;
+            batero.css('left', (piracyScreen + (restScreen / 1.5)) - (anchoBatero / 2) + 'px');
+
+        } else if (currentPiracy > 40) {
             //mirando costado
+            if (step != 2) {
+                step = 2;
+                cantante.f().src = '/images/cantante2.png';
+                bajista.f().src = '/images/bajista2.png';
+
+            }
+
+            var anchoCantante = cantante.f().offsetWidth;
+            cantante.css('left', piracyScreen + 'px');
+
+            var anchoBajista = bajista.f().offsetWidth;
+            bajista.css('left', (piracyScreen + (restScreen / 2.5)) - (anchoBajista / 2) + 'px');
+
+            var anchoBatero = batero.f().offsetWidth;
+            batero.css('left', (piracyScreen + (restScreen / 1.5)) - (anchoBatero / 2) + 'px');
 
         } else {
             //empujando pared
+            if (step != 3) {
+                step = 3;
+
+                // cantante.f().src = '/images/cantante3.png';
+                // bajista.f().src = '/images/bajista2.png';
+            }
+            var anchoCantante = cantante.f().offsetWidth;
+            cantante.css('left', piracyScreen + 'px');
+
+            var anchoBajista = bajista.f().offsetWidth;
+            bajista.css('left', (piracyScreen + (restScreen / 2.5)) + 'px');
+
+            var anchoBatero = batero.f().offsetWidth;
+            batero.css('left', (piracyScreen + (restScreen / 1.5)) + 'px');
 
         }
 
--- public/javascripts/maingame.js
+++
@@ -1,9 +0,0 @@
-function template(locals, attrs, escape, rethrow, merge) {
-attrs = attrs || jade.attrs; escape = escape || jade.escape; rethrow = rethrow || jade.rethrow; merge = merge || jade.merge;
-var buf = [];
-with (locals || {}) {
-var interp;
-buf.push('<div class="start"><h2>Click to start</h2><img src="/images/click.png"/></div><div class="piracy"></div><div class="persons"></div>');
-}
-return buf.join("");
-}
